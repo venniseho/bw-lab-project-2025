9378710100f8344422c48be2764d4e42d97277f5
Ho Vennise Hiu Tung <hovennis@teach.cs.toronto.edu>
2026-01-29 11:35:31 -0500

    refactoring file structure and also created iou_scatterplot metric

--- analysis/iou_scatterplot.py
+++ analysis/iou_scatterplot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load data from the CSV file into a Pandas DataFrame
-df = pd.read_csv('outputs/tests/manual_coco_check/metrics/sam_iou.csv')
+df = pd.read_csv('outputs/metrics/sam_iou.csv')
 
 # Create the scatter plot
 # Specify the columns for the x and y axes
@@ -20,4 +20,4 @@ plt.ylim(0, 1)
 plt.gca().set_aspect('equal', adjustable='box')
 
 # Save the plot
-plt.savefig('outputs/tests/manual_coco_check/metrics/sam_iou_scatterplot.png')
+plt.savefig('outputs/metrics/sam_iou_scatterplot.png')
